428dff1185061abc10c562b7d5d9c26f4bbce256
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-03-24 13:25:40 +0000

    feat: use the save created to establish the connection to the database

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,14 @@
-mod database;
-
 use tokio::runtime::Runtime;
+
+mod save;
+mod database;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let save_name =  save::save_system::generate_save_name();
+
     let runtime_tokio = Runtime::new().expect("Failed create tokio runtime");
-    let database_pool = runtime_tokio.block_on(database::connection::init_database())
+    let database_pool = runtime_tokio.block_on(database::connection::init_database(&save_name))
     .expect("Failed to initialize database");
 
     tauri::Builder::default()
