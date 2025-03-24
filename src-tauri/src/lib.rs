4317dc8a8f1042166f36652eb424135ed39da9d9
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-03-24 12:19:10 +0000

    feat: start the connection to the database

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,16 @@
 mod database;
 
+use tokio::runtime::Runtime;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    database::connection::connect_database();
+    let runtime_tokio = Runtime::new().expect("Failed create tokio runtime");
+    let database_pool = runtime_tokio.block_on(database::connection::init_database())
+    .expect("Failed to initialize database");
 
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .manage(database_pool)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
