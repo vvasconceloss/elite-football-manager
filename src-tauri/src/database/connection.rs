6a27c3db500dbbe3d017b6ff7ba1e1b1c37b0607
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-03-24 11:51:26 +0000

    feat: return the full path of the database

--- src-tauri/src/database/connection.rs
+++ src-tauri/src/database/connection.rs
@@ -5,7 +5,7 @@ fn fetch_config_path() -> Option<PathBuf> {
     BaseDirs::new().map(|base_dirs| base_dirs.config_dir().to_path_buf())
 }
 
-fn create_database_files(config_path: PathBuf) -> std::io::Result<()> {
+fn create_database_files(config_path: PathBuf) -> std::io::Result<PathBuf> {
     let user_config_path = config_path.to_str().ok_or_else(||
         io::Error::new(
             io::ErrorKind::InvalidData, 
@@ -20,17 +20,17 @@ fn create_database_files(config_path: PathBuf) -> std::io::Result<()> {
     fs::create_dir_all(database_path.parent().unwrap())?;
     File::create(&database_path).ok();
 
-    Ok(())
+    Ok(database_path)
 }
 
-pub fn connect_database() {
+pub async fn connect_database() -> Result<PathBuf, String> {
     match fetch_config_path() {
         Some(config_path) => {
-            match create_database_files(config_path) {
-                Ok(()) => {},
-                Err(e) => eprintln!("Failed to create the file: {}", e),
+            match create_database_files(config_path.clone()) {
+                Ok(database_path) => Ok(database_path),
+                Err(e) => Err(format!("Failed to create the file: {}", e))
             }
         }
-        None => eprintln!("Error: Configuration directory not found"),
+        None => Err("Configuration directory not found".to_string())
     }
 }
